d1e208fafbd1b7e51ebae3e41f13a7e39aec4d56
Ignacio Kairuz <ikairuz@fi.uba.ar>
2021-05-08 13:45:53 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,7 +14,7 @@
 		settings = {
 
 			// Parallax background effect?
-				parallax: false,
+				parallax: ,
 
 			// Parallax factor (lower = more intense, higher = less intense).
 				parallaxFactor: 20
